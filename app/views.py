70eb7fd596829d6b7eb16924cb27939d631b3ce1
mohammedfathaah <mohammedfathaah@gmail.com>
2025-12-17 13:38:34 +0530

    data received at the db

--- app/views.py
+++ app/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
 from .forms import *
-
+from .models import *
 # Create your views here.
 def bforms1(request):
     if request.method=='POST':
         form = ContactForm(request.POST)
+        if form.is_valid():
+            name=form.cleaned_data['name']
+            email=form.cleaned_data['email']
+            message=form.cleaned_data['message']
+            data=Contact.objects.create(name=name,email=email,message=message)
+            data.save()
     else:
         form=ContactForm()
     return render(request,'form.html',{'form':form})
