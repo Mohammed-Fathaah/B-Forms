cfd62cdcc706cfdf5c6d35e95dc70f3aea77d3dd
mohammedfathaah <mohammedfathaah@gmail.com>
2025-12-17 13:47:10 +0530

    user defined form was completed

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,11 @@
 from django import forms
+from .models import *
+# class ContactForm(forms.Form):
+#     name=forms.CharField(max_length=100)
+#     email=forms.EmailField()
+#     message=forms.CharField(widget=forms.Textarea)
 
-class ContactForm(forms.Form):
-    name=forms.CharField(max_length=100)
-    email=forms.EmailField()
-    message=forms.CharField(widget=forms.Textarea)
+class modelContactform(forms.ModelForm):
+    class Meta:
+        model=Contact
+        fields='__all__'
--- app/views.py
+++ app/views.py
@@ -4,13 +4,14 @@ from .models import *
 # Create your views here.
 def bforms1(request):
     if request.method=='POST':
-        form = ContactForm(request.POST)
+        form = modelContactform(request.POST)
         if form.is_valid():
-            name=form.cleaned_data['name']
-            email=form.cleaned_data['email']
-            message=form.cleaned_data['message']
-            data=Contact.objects.create(name=name,email=email,message=message)
-            data.save()
+            # name=form.cleaned_data['name']
+            # email=form.cleaned_data['email']
+            # message=form.cleaned_data['message']
+            # data=Contact.objects.create(name=name,email=email,message=message)
+            # data.save()
+            form.save()
     else:
-        form=ContactForm()
+        form=modelContactform()
     return render(request,'form.html',{'form':form})
